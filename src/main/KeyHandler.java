7ad7c4acb09797781685119a1b204f4ec4a88060
Rogério Domingos <rogerio.ds@hotmail.com>
2026-02-10 17:13:55 -0300

    changed KeyHandler to a better switch approach

--- src/main/KeyHandler.java
+++ src/main/KeyHandler.java
@@ -4,51 +4,50 @@ import java.awt.event.KeyEvent;
 import java.awt.event.KeyListener;
 
 public class KeyHandler implements KeyListener {
-	
+
 	public boolean upPressed, downPressed, leftPressed, rightPressed;
 
 	@Override
-	public void keyTyped(KeyEvent e) {}
+	public void keyTyped(KeyEvent e) {
+	}
 
 	@Override
 	public void keyPressed(KeyEvent e) {
-		int code = e.getKeyCode();
-		
-		if (code == KeyEvent.VK_W) {
-			upPressed = true;
-		}
-		
-		if (code == KeyEvent.VK_S) {
-			downPressed = true;
-		}
-
-		if (code == KeyEvent.VK_A) {
-			leftPressed = true;	
-		}
-
-		if (code == KeyEvent.VK_D) {
-			rightPressed = true;
+		switch (e.getKeyCode()) {
+			case KeyEvent.VK_W:
+				upPressed = true;
+				break;
+			case KeyEvent.VK_S:
+				downPressed = true;
+				break;
+			case KeyEvent.VK_A:
+				leftPressed = true;
+				break;
+			case KeyEvent.VK_D:
+				rightPressed = true;
+				break;
+			default:
+				break;
 		}
 	}
 
 	@Override
 	public void keyReleased(KeyEvent e) {
-		int code = e.getKeyCode();
-		
-		if (code == KeyEvent.VK_W) {
-			upPressed = false;
-		}
-		
-		if (code == KeyEvent.VK_S) {
-			downPressed = false;
-		}
-
-		if (code == KeyEvent.VK_A) {
-			leftPressed = false;	
-		}
-
-		if (code == KeyEvent.VK_D) {
-			rightPressed = false;
+		switch (e.getKeyCode()) {
+			case KeyEvent.VK_W:
+				upPressed = false;
+				break;
+			case KeyEvent.VK_S:
+				downPressed = false;
+				break;
+			case KeyEvent.VK_A:
+				leftPressed = false;
+				break;
+			case KeyEvent.VK_D:
+				rightPressed = false;
+				break;
+			default:
+				break;
 		}
 	}
 
